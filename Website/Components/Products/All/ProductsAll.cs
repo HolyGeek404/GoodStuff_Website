0d2f85ea9360c46fb79372de9bd80f853f73b914
HolyGeek404 <wmatkowski404@proton.me>
2025-07-10 14:09:46 +0200

    Added logic that creates an object represents selected filters. Turned off browser launch. Added InteractiveServer as default render mode

--- Website/Components/Products/All/ProductsAll.cs
+++ Website/Components/Products/All/ProductsAll.cs
@@ -8,7 +8,7 @@ public partial class ProductsAll : ComponentBase
     [Inject] IProductService productService { get; set; }
     [Inject] IProductFilterService productFilterService { get; set; }
     [Parameter] public string Category { get; set; }
-    [SupplyParameterFromForm] public string selectedFilters { get; set; }
+    public Dictionary<string, List<string>> selectedFilters = [];
 
     private IProductFilterService filterService { get; set; }
     public List<Dictionary<string, string>> Model { get; set; }
@@ -20,8 +20,30 @@ public partial class ProductsAll : ComponentBase
         Filters = productFilterService.GetFilters(Model, Category);
     }
 
+    private void UpdateFilters(string type, string value, ChangeEventArgs e)
+    {
+        if (selectedFilters.TryGetValue(type, out var filterList))
+        {
+            if ((bool)e.Value!)
+            {
+                if (!filterList.Contains(value))
+                {
+                    filterList.Add(value);
+                }
+            }
+            else
+            {
+                filterList.Remove(value);
+            }
+        }
+        else
+        {
+            selectedFilters.Add(type, [value]);
+        }
+    }
+
     private void Filter()
     {
-        var filteredProducts = productFilterService.Filter(Model,selectedFilters, Category);
+        // var filteredProducts = productFilterService.Filter(Model,selectedFilters, Category);
     }
 }
