ae36f475afab0ba588d61f29aa6e1368afa8803c
HolyGeek404 <wmatkowski404@proton.me>
2025-10-02 16:12:37 +0200

    Design changes for UserSession.cs - this will be improved in the feature

--- GoodStuff.Website.Domain/Entities/User/UserSession.cs
+++ GoodStuff.Website.Domain/Entities/User/UserSession.cs
@@ -2,9 +2,9 @@ namespace GoodStuff.Website.Domain.Entities.User;
 
 public class UserSession
 {
-    public User UserData { get; set; }
-    public List<string> Roles { get; set; } = new();
+    public User UserData { get; init; } = null!;
+    public List<string> Roles { get; set; } = [];
     public DateTime LoginTime { get; set; }
     public DateTime LastActivity { get; set; }
-    public string IpAddress { get; set; }
+    public string IpAddress { get; init; } =  string.Empty;
 }
